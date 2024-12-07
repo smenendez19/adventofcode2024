27d75c8af1a769e7f8392debc5e9e9d03ae3b8cf
Santiago Menendez <santiagomenendez@outlook.com>
2024-12-06 21:49:25 -0300

    Optimize part two day 6

--- day06/day06.go
+++ day06/day06.go
@@ -19,11 +19,10 @@ func findGuard(lab []string) (int, int) {
 	return -1, -1
 }
 
-func drawChar(lab []string, x int, y int, c rune) []string {
+func drawChar(lab []string, x int, y int, c rune) {
 	out := []rune(lab[x])
 	out[y] = c
 	lab[x] = string(out)
-	return lab
 }
 
 func drawLab(lab []string) {
@@ -56,7 +55,7 @@ func countX(lab []string) int {
 	return count
 }
 
-func markPositions(lab []string) []string {
+func markPositions(lab []string) {
 	guardX, guardY := findGuard(lab)
 	view := '^'
 	for guardX > 0 && guardY > 0 && guardX < len(lab)-1 && guardY < len(lab[0])-1 {
@@ -71,14 +70,12 @@ func markPositions(lab []string) []string {
 			guardY--
 		} else {
 			view = rotateGuard(view)
-			lab = drawChar(lab, guardX, guardY, view)
+			drawChar(lab, guardX, guardY, view)
 			continue
 		}
-		lab = drawChar(lab, guardX, guardY, view)
-		lab = drawChar(lab, prevX, prevY, 'X')
+		drawChar(lab, guardX, guardY, view)
+		drawChar(lab, prevX, prevY, 'X')
 	}
-
-	return lab
 }
 
 func checkObstacleForward(lab []string, x int, y int, view rune, obstacle string) bool {
@@ -125,11 +122,11 @@ func checkCycle(lab []string) bool {
 				}
 			}
 			view = rotateGuard(view)
-			lab = drawChar(lab, guardX, guardY, view)
+			drawChar(lab, guardX, guardY, view)
 			continue
 		}
-		lab = drawChar(lab, guardX, guardY, view)
-		lab = drawChar(lab, prevX, prevY, 'X')
+		drawChar(lab, guardX, guardY, view)
+		drawChar(lab, prevX, prevY, 'X')
 	}
 
 	return false
@@ -140,9 +137,9 @@ func findCycles(lab []string) int {
 	tries := 0
 	for i := 0; i < len(lab); i++ {
 		for j := 0; j < len(lab[i]); j++ {
-			if string(lab[i][j]) == "." {
+			if string(lab[i][j]) == "X" {
 				newLab := append([]string(nil), lab...)
-				newLab = drawChar(newLab, i, j, 'O')
+				drawChar(newLab, i, j, 'O')
 				if checkCycle(newLab) {
 					cycles++
 				}
@@ -170,11 +167,18 @@ func main() {
 
 	// Part one
 	positions := 0
-	labPartOne := markPositions(lab)
+	labPartOne := append([]string(nil), lab...)
+	markPositions(labPartOne)
 	positions = countX(labPartOne)
 	fmt.Println("Distinct positions:", positions)
 
-	// Part two
-	cycles := findCycles(lab)
+	// Find guard initial position and replace in the marked lab
+	guardX, guardY := findGuard(lab)
+	prevGuardX, prevGuardY := findGuard(labPartOne)
+	drawChar(labPartOne, guardX, guardY, '^')
+	drawChar(labPartOne, prevGuardX, prevGuardY, 'X')
+
+	// Part two (reutilize X from part One)
+	cycles := findCycles(labPartOne)
 	fmt.Println("Cycles:", cycles)
 }
