618b3dc987d840055305a0fd2f485e1095898b70
Santiago Menendez <santiagomenendez@outlook.com>
2024-12-06 21:29:23 -0300

    Add part two

--- day06/day06.go
+++ day06/day06.go
@@ -19,9 +19,9 @@ func findGuard(lab []string) (int, int) {
 	return -1, -1
 }
 
-func drawX(lab []string, x int, y int) []string {
+func drawChar(lab []string, x int, y int, c rune) []string {
 	out := []rune(lab[x])
-	out[y] = 'X'
+	out[y] = c
 	lab[x] = string(out)
 	return lab
 }
@@ -32,23 +32,16 @@ func drawLab(lab []string) {
 	}
 }
 
-func drawGuard(lab []string, x int, y int, view rune) []string {
-	out := []rune(lab[x])
-	out[y] = view
-	lab[x] = string(out)
-	return lab
-}
-
-func rotateGuard(view string) string {
+func rotateGuard(view rune) rune {
 	switch view {
-	case "^":
-		return ">"
-	case ">":
-		return "v"
-	case "v":
-		return "<"
-	case "<":
-		return "^"
+	case '^':
+		return '>'
+	case '>':
+		return 'v'
+	case 'v':
+		return '<'
+	case '<':
+		return '^'
 	default:
 		return view
 	}
@@ -65,31 +58,99 @@ func countX(lab []string) int {
 
 func markPositions(lab []string) []string {
 	guardX, guardY := findGuard(lab)
-	view := "^"
+	view := '^'
 	for guardX > 0 && guardY > 0 && guardX < len(lab)-1 && guardY < len(lab[0])-1 {
 		prevX, prevY := guardX, guardY
 		if string(lab[guardX][guardY]) == "^" && slices.Contains([]string{".", "X"}, string(lab[guardX-1][guardY])) {
-			view = "^"
 			guardX--
 		} else if string(lab[guardX][guardY]) == "v" && slices.Contains([]string{".", "X"}, string(lab[guardX+1][guardY])) {
-			view = "v"
 			guardX++
 		} else if string(lab[guardX][guardY]) == ">" && slices.Contains([]string{".", "X"}, string(lab[guardX][guardY+1])) {
-			view = ">"
 			guardY++
 		} else if string(lab[guardX][guardY]) == "<" && slices.Contains([]string{".", "X"}, string(lab[guardX][guardY-1])) {
-			view = "<"
 			guardY--
 		} else {
 			view = rotateGuard(view)
-			lab = drawGuard(lab, guardX, guardY, rune(view[0]))
+			lab = drawChar(lab, guardX, guardY, view)
 			continue
 		}
-		lab = drawGuard(lab, guardX, guardY, rune(view[0]))
-		lab = drawX(lab, prevX, prevY)
+		lab = drawChar(lab, guardX, guardY, view)
+		lab = drawChar(lab, prevX, prevY, 'X')
 	}
 
 	return lab
+}
+
+func checkObstacleForward(lab []string, x int, y int, view rune, obstacle string) bool {
+	switch view {
+	case '^':
+		return string(lab[x-1][y]) == obstacle
+	case 'v':
+		return string(lab[x+1][y]) == obstacle
+	case '>':
+		return string(lab[x][y+1]) == obstacle
+	case '<':
+		return string(lab[x][y-1]) == obstacle
+	default:
+		return false
+	}
+}
+
+func checkCycle(lab []string) bool {
+	guardX, guardY := findGuard(lab)
+	var crashesMap map[string]int
+	crashesMap = make(map[string]int)
+	view := '^'
+	for guardX > 0 && guardY > 0 && guardX < len(lab)-1 && guardY < len(lab[0])-1 {
+		prevX, prevY := guardX, guardY
+		if string(lab[guardX][guardY]) == "^" && slices.Contains([]string{".", "X"}, string(lab[guardX-1][guardY])) {
+			guardX--
+		} else if string(lab[guardX][guardY]) == "v" && slices.Contains([]string{".", "X"}, string(lab[guardX+1][guardY])) {
+			guardX++
+		} else if string(lab[guardX][guardY]) == ">" && slices.Contains([]string{".", "X"}, string(lab[guardX][guardY+1])) {
+			guardY++
+		} else if string(lab[guardX][guardY]) == "<" && slices.Contains([]string{".", "X"}, string(lab[guardX][guardY-1])) {
+			guardY--
+		} else {
+			if checkObstacleForward(lab, guardX, guardY, view, "O") || checkObstacleForward(lab, guardX, guardY, view, "#") {
+				key := string(guardX) + "," + string(guardY)
+				crashes, ok := crashesMap[key]
+				if !ok {
+					crashesMap[key] = 1
+				} else {
+					crashesMap[key]++
+					if crashes > 1 {
+						return true
+					}
+				}
+			}
+			view = rotateGuard(view)
+			lab = drawChar(lab, guardX, guardY, view)
+			continue
+		}
+		lab = drawChar(lab, guardX, guardY, view)
+		lab = drawChar(lab, prevX, prevY, 'X')
+	}
+
+	return false
+}
+
+func findCycles(lab []string) int {
+	cycles := 0
+	tries := 0
+	for i := 0; i < len(lab); i++ {
+		for j := 0; j < len(lab[i]); j++ {
+			if string(lab[i][j]) == "." {
+				newLab := append([]string(nil), lab...)
+				newLab = drawChar(newLab, i, j, 'O')
+				if checkCycle(newLab) {
+					cycles++
+				}
+				tries++
+			}
+		}
+	}
+	return cycles
 }
 
 func main() {
@@ -109,10 +170,11 @@ func main() {
 
 	// Part one
 	positions := 0
-	lab = markPositions(lab)
-
-	positions = countX(lab)
+	labPartOne := markPositions(lab)
+	positions = countX(labPartOne)
 	fmt.Println("Distinct positions:", positions)
 
 	// Part two
+	cycles := findCycles(lab)
+	fmt.Println("Cycles:", cycles)
 }
